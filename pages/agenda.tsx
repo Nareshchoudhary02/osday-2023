0d0871efc6a06561806134d31a4b59ddc918d210
404answernotfound <404answernotfound.public@gmail.com>
2023-01-25 16:37:29 +0100

    feature/157 First speaker hero version

--- pages/agenda.tsx
+++ pages/agenda.tsx
@@ -3,6 +3,7 @@ import { useTranslations } from 'next-intl';
 import { ReactNode } from 'react';
 import { TTalkCard } from '../constants';
 import TalkCard from '../components/TalkCard';
+import SpeakerHero from '../components/SpeakerHero';
 
 export async function getStaticProps({ locale }: { locale: any }) {
   return {
@@ -51,25 +52,30 @@ export default function Agenda() {
           }}
           originals={false}
           mainCta={{
-            text: t('maincta_text'),
-            link: 'https://sessionize.com/opensourceday23'
-          }}
-          secondaryCta={{
             text: t('secondarycta_text'),
             link: 'mailto:osday@schrodinger-hat.it'
           }}
           showTicketBtn
         />
 
-        <section className="talks_2021">
+{/*         <section className="talks_2021">
           <div className="talks_container">
             {agendaInfo.map((talk: TTalkCard) => {
               return <TalkCard key={talk.id} {...talk} />;
             })}
           </div>
+        </section> */}
+
+        <section className="talks_2021">
+          <SpeakerHero
+            title="The unknown world of Physics and Quantums"
+            name="Erwin Schrodinger"
+            time="Friday, 10:00AM"
+            talk="Fight an alligator and win. Eat a rug and furry furry hairs everywhere oh no human coming lie on counter don't get off counter i like fish but scamper this cat happen now, it was too purr-fect!!! or meow to be let in scoot butt on the rug trip on catnip."
+          />
         </section>
 
-        <section className="after_main">
+{/*         <section className="after_main">
           <h2>
             {t.rich('heading', {
               link: (children: ReactNode) => (
@@ -87,7 +93,7 @@ export default function Agenda() {
               )
             })}
           </h3>
-        </section>
+        </section> */}
       </div>
     </>
   );
